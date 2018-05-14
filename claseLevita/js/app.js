0e27a54749294a0ee6cfaeb619f508f2e10b3a83
g2Monik <g2monik@gmail.com>
2018-05-14 11:34:23 -0500

    Se sube la carpeta de trabajos en laboratoria

---
+++ claseLevita/js/app.js
@@ -0,0 +1,57 @@
+window.createBoard = function(element,width,height) {
+ var board = {
+   width:width,
+   height:height,
+   element:element,
+   grill:[],
+  }
+for (y=0; y< height; y++){
+  var row = document.createElement('div');
+  row.className='row';
+  element.appendChild(row);
+  board.grill[y] = [];
+
+  for (x=0; x< width; x++){
+    var cell = document.createElement('span');
+    cell.className='cell';
+    row.appendChild(cell);
+    board.grill [y][x] = cell;
+    }
+  }
+  return board;
+}
+
+window.createPlayer = function(x,y,color,board){
+ var player ={
+   x:x,
+   y:y,
+   color:color,
+   board:board,
+ }
+
+ player.draw= function(){
+   player.board.grill[player.y][player.x].className = player.color;
+ }
+player.erase = function(){
+  player.board.grill[player.y][player.x].className="";
+}
+
+ player.move = function(x,y){
+   var newX = player.x+x;
+   var newY = player.y +y;
+   if (newX >= player.board.width || newX < 0){
+     return false;
+   }
+   if (newY >= player.board.height || newY < 0){
+     return false;
+   }
+   player.erase();
+   player.x +=x;
+   player.y += y;
+   player.draw();
+
+   return true;
+ }
+ player.draw();
+ return player;
+}
---
+++ claseLevita/js/index.js
@@ -0,0 +1,28 @@
+window.onload = function(){
+var board = createBoard(document.getElementsByTagName('main')[0],11,11);
+var player = createPlayer(2,3,"blue",board);
+//player.draw;
+
+//player.move(1,4);
+
+
+window.player= player;
+//window.board = board;
+window.addEventListener('keypress', function(e){
+  console.log(e);
+  switch(e.key){
+    case "w":
+    player.move(0,-1);
+    break;
+    case "s":
+    player.move(0,1);
+    break;
+    case "a":
+    player.move(-1,0);
+    break;
+    case "d":
+    player.move(1,0);
+    break;
+  }
+});
+};
